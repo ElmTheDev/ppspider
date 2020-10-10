e45ef9a8ffa33da7edd0c23fe40692021718af12
Elmedin Turkeš <elmthedev@gmail.com>
2020-10-10 20:32:24 +0200

    f

--- src/puppeteer/Page.ts
+++ src/puppeteer/Page.ts
@@ -1,6 +1,6 @@
 import {Page as PageInterface} from "puppeteer-core";
 
 // 优化Page的引入方式，使得编辑器可以正常提示
-exports.Page = require("puppeteer/lib/Page").Page;
+exports.Page = require("puppeteer-core/lib/Page").Page;
 export declare interface Page extends PageInterface {}
 export declare abstract class Page implements PageInterface {}
